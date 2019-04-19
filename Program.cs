b603bbc2656ee67b9451d36c200b996f11f6e4c4
Abbey Brown <abbeybrownsemail@gmail.com>
2019-04-19 13:24:30 -0500

    practicing this

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThisInClasses
+{
+    public class Customer
+    {
+        // Public Properties
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsLocal { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+
+    public class DeliveryService
+    {
+        /*
+          Properties
+        */
+        public string Name { get; set; }
+
+        public string TransitType { get; set; }
+
+        /*
+          Methods
+        */
+        public void Deliver(Product product, Customer customer)
+        {
+            Console.WriteLine($"Product delivered by {this.TransitType} to {customer.FullName}");
+        }
+    }
+
+    public class Product
+    {
+        /*
+          Properties
+        */
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        /*
+          Methods
+        */
+        public void Ship(Customer customer, DeliveryService service)
+        {
+            if (!customer.IsLocal)
+            {
+                //this refers to the referred Product class
+                service.Deliver(this, customer);
+            }
+
+        }
+            public void PrintShippingReceipt (Customer customer, DeliveryService service) {
+
+                Console.WriteLine($@"
+                
+                {this.Title} was sent to {customer.FullName} by {service.Name}
+                
+                ");
+
+            }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product tinkerToys = new Product()
+            {
+                Title = "Tinker Toys",
+                Description = "You can build anything you want",
+                Price = 32.49,
+                Quantity = 25
+            };
+
+            Customer marcus = new Customer()
+            {
+                FirstName = "Marcus",
+                LastName = "Fulbright",
+                IsLocal = false
+            };
+
+            DeliveryService UPS = new DeliveryService()
+            {
+                Name = "UPS",
+                TransitType = "train"
+            };
+
+            // Ship the tinker toys to Marcus using UPS
+            tinkerToys.Ship(marcus, UPS);
+
+            tinkerToys.PrintShippingReceipt(marcus, UPS);
+
+        
+        }
+    }
+}
